a9b1f151693711b38a42299ce3abd3c8e944c578
Robertadenisa <147045566+Robertadenisa@users.noreply.github.com>
2023-10-19 13:17:43 +0300

    Add files via upload

---
+++ locatie.js
@@ -0,0 +1,26 @@
+function get_geo_position() {
+    var geo_params = {
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 0
+    };
+
+    function on_success(position) {
+        document.getElementById("latitude").innerHTML = "Latitude = " + position.coords.latitude;
+        document.getElementById("longitude").innerHTML = "Longitude = " + position.coords.longitude;
+        document.getElementById("acc").innerHTML = "Accuracy = " + position.coords.accuracy;
+        document.getElementById("altitude").innerHTML = "Altitude = " + position.coords.altitude;
+        var latlon = position.coords.latitude + "," + position.coords.longitude;
+        var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=14&size=400x300&key=YOUR_KEY";
+        document.getElementById("map").innerHTML = '<img src="' + img_url + '">';
+    }
+
+    function on_error(e) {
+        document.getElementById("text").innerHTML = e.message;
+    }
+
+    var geo = navigator.geolocation;
+    geo.getCurrentPosition(on_success, on_error, geo_params);
+}
+
+get_geo_position();
